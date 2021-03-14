bb28f418c0bdf5c9bee13d9448f3cc2ac0d2b375
Pavel Mikhailovskii <pavel.mikhailovskii@jetbrains.com>
2021-03-14 01:31:10 +0100

    Hide readline dependency

--- prompt.js
+++ prompt.js
@@ -1,7 +1,6 @@
-const readline = require('readline')
-
-
 const prompt = () => question => new Promise(resolve => {
+    const readline = require('readline')
+
     const rl = readline.createInterface({
         input: process.stdin,
         output: process.stdout,
