dcd0f528a3c94b0c2899eb57940b49f738f2433a
Pavel Mikhailovskii <pavel.mikhailovskii@jetbrains.com>
2021-03-14 09:34:43 +0100

    Don't try to use readline in browser

--- prompt.js
+++ prompt.js
@@ -1,16 +1,22 @@
-const prompt = () => question => new Promise(resolve => {
-    const readline = require('readline')
+const {isNode} = require('browser-or-node')
 
-    const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout,
-        terminal: false
-    })
+const prompt = () => question => new Promise((resolve, reject) => {
+    if (isNode) {
+        const readline = require('readline')
 
-    rl.question(question, answer => {
-        rl.close();
-        resolve(answer)
-    });
+        const rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout,
+            terminal: false
+        })
+
+        rl.question(question, answer => {
+            rl.close();
+            resolve(answer)
+        });
+    } else {
+        reject()
+    }
 })
 
 module.exports = prompt
