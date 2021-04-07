59fe5fa371f2ee191846681245bcf276e67700dc
Pavel Mikhailovskii <pavel.mikhailovskii@jetbrains.com>
2021-04-08 00:32:08 +0200

    Fix path to test.ludwig

--- common/src/test/test.js
+++ common/src/test/test.js
@@ -1,7 +1,7 @@
 const ludwig = require('../index')
 const fs = require('fs')
 
-const source = fs.readFileSync('./test.ludwig', 'utf-8')
+const source = fs.readFileSync(__dirname +'/test.ludwig', 'utf-8')
 const failed = ludwig.eval(source, 'tests')
 if (failed) {
     console.error(`${failed} tests failed`)
